757437029b900bb94efe1a3975238c222b18ca67
Michinosuche <takemonami@yahoo.com>
2023-01-31 04:14:42 +0900

    レビュー基準に沿った修正１

--- src/review01/Review01.java
+++ src/review01/Review01.java
@@ -3,14 +3,14 @@ package review01;
 public class Review01 {
 
     public static void main(String[] args) {
-        int num1 = 1200;
-        int num2 = 10;
-        double priceWithTax = tax(num1, num2);
-        System.out.println(num1 + "円の商品の税込価格は" + (int) priceWithTax + "円（消費税は" + (num1/num2) + "円）です。");
+        int num1 = 1000;
+        int num2 = 8;
+        double Tax = tax(num2);
+        System.out.println(num1 + "円の商品の税込価格は" + (int)(num1 + (num1 * Tax)) + "円（消費税は" + (int)(num1 * Tax) + "円）です。");
     }
 
-    public static double tax(int p, int t) {
-        double result = p + ((double) p / t);
+    public static double tax(int t) {
+        double result = ((double) t)/100;
         return result;
     }
 }
