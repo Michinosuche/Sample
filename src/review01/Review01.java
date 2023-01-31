cc706ac52e7e691cfb952c0bdc88d3b43ce40868
Michinosuche <takemonami@yahoo.com>
2023-02-01 06:25:08 +0900

    taxメソッドが税率を計算する仕様だったのを税額を計算するように修正

--- src/review01/Review01.java
+++ src/review01/Review01.java
@@ -5,12 +5,12 @@ public class Review01 {
     public static void main(String[] args) {
         int num1 = 1000;
         int num2 = 8;
-        double Tax = tax(num2);
-        System.out.println(num1 + "円の商品の税込価格は" + (int)(num1 + (num1 * Tax)) + "円（消費税は" + (int)(num1 * Tax) + "円）です。");
+        double Tax = tax(num1, num2);
+        System.out.println(num1 + "円の商品の税込価格は" + (int)(num1 + Tax) + "円（消費税は" + (int)(Tax) + "円）です。");
     }
 
-    public static double tax(int t) {
-        double result = ((double) t)/100;
+    public static double tax(int p, int t) {
+        double result = p * ((double) t)/100;
         return result;
     }
 }
